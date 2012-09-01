063a5e9210277acf61235c761a8e5cce4d0a4f19
Julien Coloos <julien.coloos@gmail.com>
2012-09-01 23:10:35 +0200

    Renamed project.

--- src/main/scripts/launch.cmd
+++ src/main/scripts/launch.cmd
@@ -2,7 +2,7 @@
 
 SET DIR_SCRIPT=%~dp0
 CD /D "%DIR_SCRIPT%"
-SET CLASSPATH_APP=%DIR_SCRIPT%config;%DIR_SCRIPT%lib\*;%DIR_SCRIPT%webapp\WEB-INF\lib\test-jsf-primefaces-0.1.0.jar
+SET CLASSPATH_APP=%DIR_SCRIPT%config;%DIR_SCRIPT%lib\*;%DIR_SCRIPT%webapp\WEB-INF\lib\stealthnet-ui-web-jsf-0.1.0.jar
 
 @REM http://wiki.eclipse.org/Jetty/Howto/Configure_JSP
 java -Dorg.apache.jasper.compiler.disablejsr199=true -cp "%CLASSPATH_APP%" stealthnet.scala.ui.web.sandbox.TestServer
--- src/main/scripts/launch.sh
+++ src/main/scripts/launch.sh
@@ -1,6 +1,6 @@
 dirScript=$(dirname "$0")
 cd "${dirScript}"
-CLASSPATH_APP=${dirScript}/config:${dirScript}/lib/*:${dirScript}/webapp/WEB-INF/lib/test-jsf-primefaces-0.1.0.jar
+CLASSPATH_APP=${dirScript}/config:${dirScript}/lib/*:${dirScript}/webapp/WEB-INF/lib/stealthnet-ui-web-jsf-0.1.0.jar
 
 # http://wiki.eclipse.org/Jetty/Howto/Configure_JSP
 java -Dorg.apache.jasper.compiler.disablejsr199=true -cp "${CLASSPATH_APP}" stealthnet.scala.ui.web.sandbox.TestServer
