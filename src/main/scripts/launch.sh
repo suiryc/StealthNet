29ca512c58b9c6b777ab901e47758a474ce78934
Julien Coloos <julien.coloos@gmail.com>
2013-05-19 02:45:24 +0200

    Fixed launch script

--- src/main/scripts/launch.sh
+++ src/main/scripts/launch.sh
@@ -1,5 +1,6 @@
 dirScript=$(dirname "$0")
 cd "${dirScript}"
+dirScript=$(pwd)
 CLASSPATH_APP=${dirScript}/config:${dirScript}/lib/*:${dirScript}/webapp/WEB-INF/lib/stealthnet-ui-web-jsf-0.1.0.jar
 
 # http://wiki.eclipse.org/Jetty/Howto/Configure_JSP
