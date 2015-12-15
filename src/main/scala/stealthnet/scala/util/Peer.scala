807824392da2844e6d18ade89a355c5902974c44
Julien Coloos <julien.coloos@gmail.com>
2015-12-16 00:59:08 +0100

    Revert "Try to handle IPv6 addresses received from web caches"
    WebCache does not correctly handle IPv6: address is accepted but truncated to the max size of an IPv4 one (15 chars: 4*3 digits + 3 dots)
    
    This reverts commit e97ae5023cf5bdd6ee8cec200ecb46e55a72de52.

--- src/main/scala/stealthnet/scala/util/Peer.scala
+++ src/main/scala/stealthnet/scala/util/Peer.scala
@@ -9,15 +9,8 @@ object Peer {
   /** Regular expression to extract host and port number. */
   val regexp = "^(.+):(\\d+)$".r
 
-  def apply(host: String, port: Int) = {
-    val actualHost =
-      if (!host.contains(':') || host.contains("::")) host
-      else {
-        if (host.endsWith(":")) s"$host:"
-        else s"$host::"
-      }
-    new Peer(new InetSocketAddress(InetAddress.getByName(actualHost), port))
-  }
+  def apply(host: String, port: Int) =
+    new Peer(new InetSocketAddress(InetAddress.getByName(host), port))
 
 }
 
